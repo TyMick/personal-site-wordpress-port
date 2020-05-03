771885011da0cbf88d23412460004294bf22e9c1
Ty Mick <ty@tymick.me>
2020-05-03 00:26:18 -0400

    Adjust Connect page

---
+++ page-connect.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Ty_Mick
+ */
+
+get_header();
+?>
+
+<main class="container cap-width-md mb-5">
+	<?php
+    while (have_posts()) {
+		the_post();
+		
+		the_title("<h1>", "</h1>");
+
+		the_content();
+    }
+	?>
+</main>
+
+<?php
+get_footer();
